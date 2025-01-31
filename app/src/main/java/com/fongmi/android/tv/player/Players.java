5d4684a2dc1b2ec2e34b694a6a0ae748fc499eba
xinyi1984 <106141301+xinyi1984@users.noreply.github.com>
2025-01-31 09:23:20 +0800

    Update Players.java
    Signed-off-by: xinyi1984 <106141301+xinyi1984@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/player/Players.java
+++ app/src/main/java/com/fongmi/android/tv/player/Players.java
@@ -116,7 +116,7 @@ public class Players implements Player.Listener, ParseCallback {
     }
 
     private void setPlayer(PlayerView view) {
-        exoPlayer = new ExoPlayer.Builder(App.get()).setLoadControl(ExoUtil.buildLoadControl()).setTrackSelector(ExoUtil.buildTrackSelector()).setRenderersFactory(ExoUtil.buildRenderersFactory(isHard() ? EXTENSION_RENDERER_MODE_ON : EXTENSION_RENDERER_MODE_PREFER)).setMediaSourceFactory(ExoUtil.buildMediaSourceFactory()).build();
+        exoPlayer = new ExoPlayer.Builder(App.get()).setLoadControl(ExoUtil.buildLoadControl()).setTrackSelector(ExoUtil.buildTrackSelector()).setRenderersFactory(ExoUtil.buildRenderersFactory()).setMediaSourceFactory(ExoUtil.buildMediaSourceFactory()).build();
         exoPlayer.setAudioAttributes(AudioAttributes.DEFAULT, true);
         exoPlayer.addAnalyticsListener(new EventLogger());
         exoPlayer.setHandleAudioBecomingNoisy(true);
