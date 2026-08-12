8e81f157c96871372a0a5d730df6164ac07e18fe
xinyi1984 <106141301+xinyi1984@users.noreply.github.com>
2026-08-13 04:36:08 +0800

    Update FileUtil.java
    Signed-off-by: xinyi1984 <106141301+xinyi1984@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/utils/FileUtil.java
+++ app/src/main/java/com/fongmi/android/tv/utils/FileUtil.java
@@ -2,6 +2,7 @@ package com.fongmi.android.tv.utils;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.StatFs;
 import android.text.TextUtils;
 
@@ -135,7 +136,7 @@ public class FileUtil {
     }
 
     public static Uri getShareUri(File file) {
-        return FileProvider.getUriForFile(App.get(), App.get().getPackageName() + ".provider", file);
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.N ? Uri.fromFile(file) : FileProvider.getUriForFile(App.get(), App.get().getPackageName() + ".provider", file);
     }
 
     private static String getMimeType(String fileName) {
