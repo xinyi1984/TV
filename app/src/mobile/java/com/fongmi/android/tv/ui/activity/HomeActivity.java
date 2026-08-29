d1cd3e0b4375a93df408f76f2506d6198bb92fde
xinyi1984 <106141301+xinyi1984@users.noreply.github.com>
2026-08-29 23:32:03 +0800

    Update HomeActivity.java
    Signed-off-by: xinyi1984 <106141301+xinyi1984@users.noreply.github.com>

--- app/src/mobile/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
@@ -104,7 +104,7 @@ public class HomeActivity extends BaseActivity implements NavigationBarView.OnIt
 
     private void checkType(Intent intent) {
         if ("text/plain".equals(intent.getType()) || UrlUtil.path(intent.getData()).endsWith(".m3u")) {
-            FileChooser.getUri(intent, uri -> loadLive(uri.toString()));
+            FileChooser.getUri(intent, uri -> loadLive(UrlUtil.toLocalUrl(uri)));
         } else {
             FileChooser.getUri(intent, uri -> VideoActivity.file(this, uri));
         }
